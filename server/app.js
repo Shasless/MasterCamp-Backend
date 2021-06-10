7e1823d35f0594434eb45e03860ce2306d0dca3f
EnzoFilangi <48645462+EnzoFilangi@users.noreply.github.com>
2021-06-10 11:33:40 +0200

    Enable CORS

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const session = require('express-session')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -10,10 +11,18 @@ const apiRouter = require('./routes/api.js')
 
 const app = express()
 
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true,
+};
+
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(cors(corsOptions))
 app.use(session({ secret: "rsghqn5q41hbn3d1bn521 5b b 6d041vb d6vb4 16rv416v15e1", saveUninitialized: false, resave: false }))
 app.use(express.static(path.join(__dirname, '../client')))
 
